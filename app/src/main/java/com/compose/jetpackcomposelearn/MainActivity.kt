df4f15a4ffbb05a8c030d83054e7fb523c508465
Constantine Kim <electro.passion92@gmail.com>
2025-04-28 22:56:22 +0900

    Контейнер Surface

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.compose.jetpackcomposelearn.containers.BoxExample
 import com.compose.jetpackcomposelearn.containers.ColumnExample
 import com.compose.jetpackcomposelearn.containers.RowExample
+import com.compose.jetpackcomposelearn.containers.SurfaceExample
 import com.compose.jetpackcomposelearn.ui.theme.JetpackComposeLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +36,8 @@ fun ContainerExample(modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
         item { BoxExample() }
         item { ColumnExample() }
-        item { RowExample()  }
+        item { RowExample() }
+        item { SurfaceExample() }
     }
 }
 
---
+++ app/src/main/java/com/compose/jetpackcomposelearn/containers/SurfaceContainer.kt
@@ -0,0 +1,33 @@
+package com.compose.jetpackcomposelearn.containers
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SurfaceExample() {
+    Surface(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        tonalElevation = 2.dp,
+        shadowElevation = 6.dp,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        border = BorderStroke(1.dp, Color.Gray)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("Загловок", style = MaterialTheme.typography.titleLarge)
+            Text("Контент внутри Surface")
+        }
+    }
+}
