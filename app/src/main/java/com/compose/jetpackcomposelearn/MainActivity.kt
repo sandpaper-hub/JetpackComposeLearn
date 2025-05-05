531622d6f4bdb65c422a66a16a2a571aefa951f9
Constantine Kim <electro.passion92@gmail.com>
2025-05-05 20:58:12 +0900

    Контейнер LazyVerticalStaggeredGrid

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -18,6 +18,7 @@ import com.compose.jetpackcomposelearn.containers.ColumnExample
 import com.compose.jetpackcomposelearn.containers.LazyColumnExample
 import com.compose.jetpackcomposelearn.containers.LazyHorizontalGridExample
 import com.compose.jetpackcomposelearn.containers.LazyRowExample
+import com.compose.jetpackcomposelearn.containers.LazyVerticalStaggeredGridExample
 import com.compose.jetpackcomposelearn.containers.LazyVerticalGridExample
 import com.compose.jetpackcomposelearn.containers.RowExample
 import com.compose.jetpackcomposelearn.containers.SurfaceExample
@@ -49,6 +50,7 @@ fun ContainerExample(modifier: Modifier = Modifier) {
         item { LazyRowExample() }
         item { LazyHorizontalGridExample() }
         item { LazyVerticalGridExample() }
+        item { LazyVerticalStaggeredGridExample() }
     }
 }
 
