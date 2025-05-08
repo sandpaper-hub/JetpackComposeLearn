306520d9362cb06df434b4f1775ea55e975d53dc
Constantine Kim <electro.passion92@gmail.com>
2025-05-09 00:20:44 +0900

    SimpleState

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.compose.jetpackcomposelearn.state.SimpleStateExample
 import com.compose.jetpackcomposelearn.ui.theme.JetpackComposeLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeLearnTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainFunction(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +31,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+@Suppress("FunctionName")
+fun MainFunction(modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        item { SimpleStateExample() }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
+@Suppress("FunctionName")
 fun GreetingPreview() {
     JetpackComposeLearnTheme {
-        Greeting("Android")
+        MainFunction()
     }
 }
