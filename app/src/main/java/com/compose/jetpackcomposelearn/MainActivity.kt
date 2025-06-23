4e872e5728b72d300d1b13fa5e1f1df5ce7b38d5
Constantine Kim <electro.passion92@gmail.com>
2025-06-23 12:10:28 +0800

    RadioButton example

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -19,6 +19,7 @@ import com.compose.jetpackcomposelearn.ui_components.ElevatedButtonExample
 import com.compose.jetpackcomposelearn.ui_components.FilledTonalButtonExample
 import com.compose.jetpackcomposelearn.ui_components.OutlinedButtonExample
 import com.compose.jetpackcomposelearn.ui_components.OutlinedTextFieldExample
+import com.compose.jetpackcomposelearn.ui_components.RadioButtonExample
 import com.compose.jetpackcomposelearn.ui_components.RadioGroupSample
 import com.compose.jetpackcomposelearn.ui_components.SelectableExample
 import com.compose.jetpackcomposelearn.ui_components.SimpleButtonExample
@@ -62,6 +63,7 @@ fun MainExample(modifier: Modifier = Modifier) {
         item { CheckBoxExample() }
         item { BorderComponent { SelectableExample()} }
         item { BorderComponent { RadioGroupSample() } }
+        item { BorderComponent { RadioButtonExample() } }
     }
 }
 
---
+++ app/src/main/java/com/compose/jetpackcomposelearn/ui_components/RadioButton.kt
@@ -0,0 +1,45 @@
+package com.compose.jetpackcomposelearn.ui_components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+@Suppress("FunctionName")
+fun RadioButtonExample() {
+    var selectedOption by remember { mutableStateOf("Green") }
+    val options = listOf("Green", "Yellow", "Red")
+
+    Column {
+        options.forEach { option ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clickable { selectedOption = option }
+            ) {
+                RadioButton(
+                    selected = (option == selectedOption),
+                    onClick = { selectedOption = option }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = option)
+            }
+        }
+    }
+}
