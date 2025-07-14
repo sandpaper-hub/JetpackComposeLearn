f29f2f5a845b8d5c7d63eb0ecc39c9c1cbbbb025
Constantine Kim <electro.passion92@gmail.com>
2025-07-14 09:29:55 +0900

    CustomLayout example

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.jetpackcomposelearn.customLayoutModifier.CustomModifierLayoutExample
+import com.compose.jetpackcomposelearn.customLayoutModifier.MyLayout
 import com.compose.jetpackcomposelearn.ui.theme.JetpackComposeLearnTheme
 import com.compose.jetpackcomposelearn.util.BorderComponent
 
@@ -35,7 +43,23 @@ class MainActivity : ComponentActivity() {
 @Suppress("FunctionName")
 fun MainExample(modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
-        item {BorderComponent { CustomModifierLayoutExample() }}
+        item { BorderComponent { CustomModifierLayoutExample() } }
+        item {
+            BorderComponent {
+                Box(
+                    Modifier.fillMaxWidth()
+                        .height(400.dp)
+                ) {
+                    MyLayout(spacing = 20) {
+                        Box(Modifier.size(50.dp).background(Color.DarkGray))
+                        Box(Modifier.size(50.dp).background(Color.Red))
+                        Box(Modifier.size(50.dp).background(Color.Green))
+                        Box(Modifier.size(50.dp).background(Color.Blue))
+                        Box(Modifier.size(50.dp).background(Color.Yellow))
+                    }
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/compose/jetpackcomposelearn/customLayoutModifier/CustomLayout.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/customLayoutModifier/CustomLayout.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.unit.dp
 
@@ -18,16 +19,34 @@ fun Modifier.shiftDownByHalf(): Modifier = this.then(
         layout(placeable.width, placeable.height + placeable.height / 2) {
             placeable.placeRelative(0, placeable.height + placeable.height / 2)
         }
-    }
-)
+    })
 
 @Composable
 @Suppress("FunctionName")
 fun CustomModifierLayoutExample() {
     Box(
-        modifier = Modifier
-            .size(50.dp)
-            .background(Color.Red)
-            .shiftDownByHalf()
+        modifier = Modifier.size(50.dp).background(Color.Red).shiftDownByHalf()
     )
+}
+
+@Composable
+@Suppress("FunctionName")
+fun MyLayout(modifier: Modifier = Modifier, spacing: Int = 0, content: @Composable () -> Unit) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yCoordinate = 0
+            var xCoordinate = 0
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = xCoordinate, y = yCoordinate)
+                xCoordinate += placeable.width + spacing
+                if ((xCoordinate + placeable.width) > constraints.maxWidth) {
+                    yCoordinate += placeable.height + spacing
+                    xCoordinate = 0
+                }
+            }
+        }
+    }
 }
