c98bbf6e493cde1998947b4384bc387d3e712a12
Constantine Kim <electro.passion92@gmail.com>
2025-07-21 11:45:02 +0800

    ConstraintLayout with bias example

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -6,14 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.compose.jetpackcomposelearn.containers.ConstraintLayoutExample
+import com.compose.jetpackcomposelearn.containers.ConstraintLayoutWithBiasExample
 import com.compose.jetpackcomposelearn.ui.theme.JetpackComposeLearnTheme
-import com.compose.jetpackcomposelearn.util.BorderComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Suppress("FunctionName")
 fun MainExample(modifier: Modifier = Modifier) {
-    ConstraintLayoutExample(modifier)
+    ConstraintLayoutWithBiasExample(modifier = modifier)
 }
 
 @Preview(name = "Galaxy S23 Plus", widthDp = 393, heightDp = 851)
--- app/src/main/java/com/compose/jetpackcomposelearn/containers/ConstraintLayout.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/containers/ConstraintLayout.kt
@@ -1,11 +1,14 @@
 package com.compose.jetpackcomposelearn.containers
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
@@ -13,7 +16,7 @@ import androidx.constraintlayout.compose.ConstraintLayout
 @Suppress("FunctionName")
 fun ConstraintLayoutExample(modifier: Modifier) {
     ConstraintLayout(
-        modifier = modifier.then(Modifier.fillMaxWidth().height(500.dp))
+        modifier = modifier.then(Modifier.fillMaxSize())
     ) {
         val (button, text) = createRefs()
         Button(onClick = {}, modifier = Modifier.constrainAs(button) {
@@ -24,11 +27,36 @@ fun ConstraintLayoutExample(modifier: Modifier) {
         }
 
         Text(
-            text = "Text example",
+            text = "Text example", modifier = Modifier.constrainAs(text) {
+                top.linkTo(button.bottom, margin = 16.dp)
+                start.linkTo(button.start)
+            })
+    }
+}
+
+@Composable
+@Suppress("FunctionName")
+fun ConstraintLayoutWithBiasExample(modifier: Modifier) {
+    ConstraintLayout(modifier = modifier.then(Modifier.fillMaxSize())) {
+        val (box1, box2) = createRefs()
+
+        Box(
+            modifier = Modifier.size(100.dp)
+                .background(Color.Red)
+                .constrainAs(box1) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                }
+        )
+
+        Box(
             modifier = Modifier
-                .constrainAs(text) {
-                    top.linkTo(button.bottom, margin = 16.dp)
-                    start.linkTo(button.start)
-                })
+                .size(100.dp)
+                .background(Color.Yellow)
+                .constrainAs(box2) {
+                    top.linkTo(box1.bottom, margin = 16.dp)
+                    linkTo(parent.start, parent.end, bias = 0.75f)
+                }
+        )
     }
 }
