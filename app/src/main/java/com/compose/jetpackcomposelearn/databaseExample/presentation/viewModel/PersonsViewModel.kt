302ab9ac3a2a2b67521a2e5e035ccd3dae87ab1d
sandpaper-hub <electro.passion92@gmail.com>
2025-09-28 23:42:47 +0900

    refactor

--- app/src/main/java/com/compose/jetpackcomposelearn/databaseExample/presentation/viewModel/PersonsViewModel.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/databaseExample/presentation/viewModel/PersonsViewModel.kt
@@ -1,6 +1,5 @@
 package com.compose.jetpackcomposelearn.databaseExample.presentation.viewModel
 
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
