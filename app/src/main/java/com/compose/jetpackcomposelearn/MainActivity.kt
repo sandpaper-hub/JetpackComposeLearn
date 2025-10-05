2d081d4940be4469a938c796e35a82049b61bc85
sandpaper-hub <electro.passion92@gmail.com>
2025-10-06 03:15:22 +0900

    Add navigation Add DetailScreen Refactor

--- app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.compose.jetpackcomposelearn.databaseExample.presentation.ui.personScreen.PersonsScreen
 import com.compose.jetpackcomposelearn.navigation.RootNavGraph
 import com.compose.jetpackcomposelearn.ui.theme.JetpackComposeLearnTheme
 import dagger.hilt.android.AndroidEntryPoint
