c6b72ad6b94b1f5a6a1472d41758091b5f71b625
sandpaper-hub <electro.passion92@gmail.com>
2025-10-12 20:49:48 +0900

    refactor

--- app/src/main/java/com/compose/jetpackcomposelearn/databaseExample/presentation/ui/CommonPersonDataInput.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/databaseExample/presentation/ui/CommonPersonDataInput.kt
@@ -1,11 +1,9 @@
 package com.compose.jetpackcomposelearn.databaseExample.presentation.ui
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
--- app/src/main/java/com/compose/jetpackcomposelearn/databaseExample/presentation/ui/detailScreen/DetailScreen.kt
+++ app/src/main/java/com/compose/jetpackcomposelearn/databaseExample/presentation/ui/detailScreen/DetailScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -30,7 +29,6 @@ fun DetailScreen(
     onBackNavigation: () -> Unit,
     onOpenEdit: (personId: Long) -> Unit
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val ui by viewModel.ui.collectAsStateWithLifecycle()
 
     var id by rememberSaveable(ui.data?.id) { mutableLongStateOf(0) }
